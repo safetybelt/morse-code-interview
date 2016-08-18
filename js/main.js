2e721c45e5c27a8da6402cb3fec81976aee49c47
Jonathan Neely <jneely@gmail.com>
2016-08-17 21:31:51 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Morse Code Translator
+ *
+ * Feel free to use any process for differentiating the space between letters and words
+ *     I recommend something like - a space between letters, a / between words; ie '-.. --- --./-.- .- -' => 'DOG CAT'
+ */
+
+function translate(text) {
+    return text;
+}
