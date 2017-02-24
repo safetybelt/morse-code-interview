c8bbf64443ef33858797df4b33520b8e6b1229d8
Jonathan Neely <jneely@gmail.com>
2017-02-24 15:11:07 -0800

    Updated the JS to include basic handling of getting the input and setting the output - interview question should be more about the actual coding and not getting/setting DOM elements

---
+++ README.md
@@ -0,0 +1,9 @@
+# Morse Code Translator
+
+This is a basic interview question involving translating plain text to morse code.
+
+To get started, run:
+
+`git clone https://github.com/safetybelt/morse-code-interview.git`
+
+You can then open `index.html` in a browser and everything should be hooked up already.  The translation process is up to you.
--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 /**
  * Morse Code Translator
  *
+ * Letters should be broken up with a space.
  * Feel free to use any process for differentiating the space between letters and words
- *     I recommend something like - a space between letters, a / between words; ie '-.. --- --./-.- .- -' => 'DOG CAT'
+ *     I recommend something like a / or | between words; ie 'DOG CAT' => '-.. --- --. / -.- .- -'
  */
 
 function translate(text) {
     return text;
 }
+
+function setOutput() {
+    var input = document.getElementById("text-to-convert");
+    var output = document.getElementById("result-content");
+
+    output.innerHTML = translate(input.value);
+}
+
+var el = document.getElementById("submit-button");
+el.addEventListener("click", setOutput);
