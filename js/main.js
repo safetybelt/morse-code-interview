9283aee5e3bf651dd2b47eee6f55fcead14f2dcb
Jonathan Neely <jneely@gmail.com>
2020-05-27 12:16:32 -0700

    Updated interview question - cleaned up confusing functions and added API URL to header comments

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
 /**
  * Morse Code Translator
+ * 
+ * Use the Letters API built for this interview:
+ * https://safetybelt.pythonanywhere.com/letters/<LETTER>
+ * 
+ *  For example, a GET call to https://safetybelt.pythonanywhere.com/letters/s will return:
+ *      {
+ *          "code": "..."
+ *      }
+ * 
+ * The API only handles one letter at a time
  *
  * Letters should be broken up with a space.
  * Feel free to use any process for differentiating the space between letters and words
- *     I recommend something like a / or | between words; ie 'DOG CAT' => '-.. --- --. / -.- .- -'
+ *     I recommend something like a / or | between words; ie 'dog cat' => '-.. --- --. / -.- .- -'
  *
  * Also feel free to modify this code or the HTML as much as needed.  You do not need to use this structure.
  */
 
-function setOutput(input) {
-    var output = document.getElementById('result-content');
+function generateOutput(input) {
+    const output = document.getElementById('result-content');
 
-    // do translating here
+    // translate here
 
     output.innerHTML = input;
 }
 
-function getInput() {
-    var input = document.getElementById('text-to-convert');
-    setOutput(input.value);
-}
-
 document.getElementById('text-input').addEventListener('submit', function(e) {
     e.preventDefault();
-    getInput();
+    const input = document.getElementById('text-to-convert');
+    generateOutput(input);
 });
